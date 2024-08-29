59d6d175e111f6fa07f55a1d7a6634e8e6ca2e06
Arif S <asrgmail.com>
2024-08-29 15:48:56 +0700

    inisial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//mengambil dari id
+let judul = document.getElementById("main-heading");
+console.log(judul);
+
+
+//mengambil dari class
+
+let kelasItems = document.getElementsByClassName("list-items");
+console.log(kelasItems);
+
+
+//mengambil dari tag
+let tagItems = document.getElementsByTagName("li");
+console.log(tagItems);
+
+
+//mengambil dari selecter (satu)
+let selectorItems = document.querySelector("li");
+console.log(selectorItems);
+
+
+//mengambil dari selector (semua)
